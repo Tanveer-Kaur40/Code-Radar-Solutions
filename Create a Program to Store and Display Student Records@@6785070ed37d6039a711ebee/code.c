56edd41498ca7df2b9e4afa82469caceb1edc9a3
Tanveer Kaur <virktanveer71@gmail.com>
2025-03-10 19:02:37 +0530

    Solution to Create a Program to Store and Display Student Records

---
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct student{
+    int rollnumber;
+    char name;
+    float marks;
+};
+int main(){
+    int N;
+    scanf("%d",&N);
+    struct student students[N];
+    for(int i=1;i<=N;i++){
+        scanf("%d %s %.2f",students[i].rollnumber, students[i].name, students[i].marks);
+    }
+    for(int i=1;i<=N;i++){
+        printf("Roll Number: %d, Name: %s, Marks: %.2f",students[i].rollnumber, students[i].name, students[i].marks);
+    }
+    return 0;
+}
