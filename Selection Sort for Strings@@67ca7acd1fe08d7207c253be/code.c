0fc9c889b5945d883cf18dd6d407880ba1cb1249
Tanveer Kaur <virktanveer71@gmail.com>
2025-03-28 14:15:05 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-void selectionSort(char arr[][20], int n){
+
+void selectionSort(char arr[][20], int n) { 
     int minIndex;
-    char temp[20];
-    for(int i=0;i<n-1;i++){
+    char temp[20]; 
+
+    for (int i = 0; i < n - 1; i++) {
         minIndex = i;
-    for(int j= i+1;j<n;j++){
-        if(strcmp(arr[j], arr[minIndex]) < 0){
-            minIndex = j;
+
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[minIndex]) < 0) { 
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            strcpy(temp, arr[i]); 
+            strcpy(arr[i], arr[minIndex]);
+            strcpy(arr[minIndex], temp);
         }
     }
-    if (minIndex != i) {
-    strcpy(temp, arr[i]);
-    strcmp(arr[i], arr[minIndex]);
-    strcmp(arr[minIndex], temp);
-    }
+}
+
+void printArray(char arr[][20], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s ", arr[i]); 
     }
 }
-void printArray(char arr[][20], int n){
-    for(int i=0;i<n;i++){
-        printf("%s ",arr[i]);
-    }
-}
+
