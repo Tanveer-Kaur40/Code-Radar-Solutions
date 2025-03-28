a977bdbb0a136df1e7259ae0fb4374a19eef169f
Tanveer Kaur <virktanveer71@gmail.com>
2025-03-29 01:25:31 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
-void movePosition(int N,int arr[], int K){
-    int temp;
-    for(int i=0;i<N;i++){
-    arr[i] = arr[k];
+void movePosition(int N, int arr[], int K) {
+    int temp[N]; 
+    K = K % N;
+
+    for (int i = 0; i < K; i++) {
+        temp[i] = arr[N - K + i];
+    }
+
+    for (int i = N - 1; i >= K; i--) {
+        arr[i] = arr[i - K];
+    }
+    for (int i = 0; i < K; i++) {
+        arr[i] = temp[i];
     }
 }
-int printArray(int arr[], int N){
-    for(int i=0;i<n;i++){
-        printf("%d",arr[i]);
+
+
+void printArray(int arr[], int N) {
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
 }
-int main(){
+
+int main() {
     int N;
-    scanf("%d",&N);
-    int arr[n];
-    for(int i=0;i<N;i++){
-        scanf("%d ",&arr[i]);
+    scanf("%d", &N);
+    
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]); // Fixed scanf
     }
+
     int K;
-    scanf("%d",&K);
+    scanf("%d", &K);
+
+    movePosition(N, arr, K);  // Fixed function call
+    printArray(arr, N);       // Fixed function call
 
     return 0;
 }
