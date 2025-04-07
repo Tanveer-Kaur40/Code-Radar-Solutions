693c6a3c7c7d88eae4cf1648961267c798773d1f
Tanveer Kaur <virktanveer71@gmail.com>
2025-04-08 02:12:01 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Function to perform Insertion Sort
 void insertionSort(int arr[], int n) {
     int i, key, j;
     for (i = 1; i < n; i++) {
@@ -9,20 +10,22 @@ void insertionSort(int arr[], int n) {
         // Move elements that are greater than key
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
-
         arr[j + 1] = key;
     }
 }
 
-void printArray(char arr[], int n) {
-    int i;
-    for(i = 0; i < n; i++) {
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
+
+
+
 
 
 
