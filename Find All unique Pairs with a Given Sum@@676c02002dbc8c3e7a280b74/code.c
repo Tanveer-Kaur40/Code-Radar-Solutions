c4d149835290d8f36aa2c2fb142816af307a7d71
Tanveer Kaur <virktanveer71@gmail.com>
2025-04-09 18:23:20 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int n, target;
     scanf("%d", &n);
-
+    
     int arr[n];
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
+    
     scanf("%d", &target);
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(arr[i] + arr[j] == target) {
-                printf("%d %d\n", arr[i], arr[j]);
+
+    // To track which pairs have been printed
+    bool printed[n][n];
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target && !printed[i][j]) {
+                // Check if the same value pair has been printed before
+                bool already_printed = false;
+                for (int x = 0; x < i; x++) {
+                    for (int y = x + 1; y < n; y++) {
+                        if ((arr[x] == arr[i] && arr[y] == arr[j]) || (arr[x] == arr[j] && arr[y] == arr[i])) {
+                            already_printed = true;
+                            break;
+                        }
+                    }
+                    if (already_printed) break;
+                }
+                if (!already_printed) {
+                    printf("%d %d\n", arr[i], arr[j]);
+                    printed[i][j] = true;
+                }
             }
         }
     }
 
     return 0;
 }
+
