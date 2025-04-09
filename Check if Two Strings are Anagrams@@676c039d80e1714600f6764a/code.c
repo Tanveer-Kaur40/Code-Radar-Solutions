0895f4e3f7ddff9086230005f806abef67869bc3
Tanveer Kaur <virktanveer71@gmail.com>
2025-04-09 18:59:48 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,17 +1,49 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str[100];
-    scanf("%s",str);
-    char n;
-    scanf(" %c",&n);
-    int len=strlen(str);
-    int count=0;
-    for(int i=0;i<len;i++){
-        if(str[i]==n){
-            count++;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX 1000
+
+// Helper function to count characters
+void countCharacters(char str[], int count[]) {
+    for (int i = 0; str[i]; i++) {
+        if (str[i] != ' ') {
+            count[tolower(str[i])]++;
         }
     }
-    printf("%d",count);
+}
+
+bool isAnagram(char str1[], char str2[]) {
+    int count1[256] = {0}, count2[256] = {0};
+
+    countCharacters(str1, count1);
+    countCharacters(str2, count2);
+
+    for (int i = 0; i < 256; i++) {
+        if (count1[i] != count2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    char str1[MAX], str2[MAX];
+
+ 
+    fgets(str1, MAX, stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+
+
+    fgets(str2, MAX, stdin);
+    str2[strcspn(str2, "\n")] = '\0';
+
+    if (isAnagram(str1, str2)) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
     return 0;
 }
