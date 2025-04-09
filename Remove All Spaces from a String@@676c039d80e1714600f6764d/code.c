ea789397fc3f3856fc59264783d71d0d37d069b3
Tanveer Kaur <virktanveer71@gmail.com>
2025-04-09 19:01:37 +0530

    Solution to Remove All Spaces from a String

---
+++ Remove All Spaces from a String@@676c039d80e1714600f6764d/code.c
@@ -0,0 +1,42 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+
+void reverseWord(char str[], int start, int end) {
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void reverseWordsInString(char str[]) {
+    int i = 0, start = 0;
+    int n = strlen(str);
+
+    while (i <= n) {
+        if (str[i] == ' ' || str[i] == '\0') {
+            reverseWord(str, start, i - 1);
+            start = i + 1;
+        }
+        i++;
+    }
+}
+
+int main() {
+    char str[1000];
+
+    // Reading input
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove trailing newline
+
+    // Process
+    reverseWordsInString(str);
+
+    // Output
+    printf("%s\n", str);
+
+    return 0;
+}
